330e9fa239558791bab6f197eda582e56c98df43
Yan Couto <yancouto@debianpc.yan.com>
2015-11-04 21:05:23 -0200

    Initial Commit

---
+++ android/src/com/mygdx/aoc/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.aoc.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.aoc.AgeOfCapybaras;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AgeOfCapybaras(), config);
+	}
+}
