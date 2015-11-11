3deb1c89303919761c873ec0c85ab6edd32c7e5d
vitorsamora <vitor.samora@gmail.com>
2015-11-11 20:30:57 -0200

    Initial ads implementation

--- android/src/com/mygdx/aoc/android/AndroidLauncher.java
+++ android/src/com/mygdx/aoc/android/AndroidLauncher.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.mygdx.aoc.AgeOfCapybaras;
 
 public class AndroidLauncher extends AndroidApplication {
+    private InterstitialAd mInterstitialAd;
+
     @Override
     protected void onPause() {
+//        Request an ad
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+//        Um id gerado para uma conta no AdMob deve ser colado no lugar de
+//        AdRequest.DEVICE_ID_EMULATOR entre aspas quando o aplicativo estiver
+//        pronto para ir para a Play Store.
+        mInterstitialAd.loadAd(adRequest);
         super.onPause();
         AgeOfCapybaras.onPause();
     }
 
     @Override
     protected void onResume() {
+//        Check that an ad is loaded and then display it
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
         super.onResume();
         AgeOfCapybaras.onResume();
     }
@@ -25,6 +41,9 @@ public class AndroidLauncher extends AndroidApplication {
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useCompass = false;
         config.useAccelerometer = false;
+//        Construct an InterstitialAd object and set its ad unit ID.
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); //Outro Id que precisa ser criado pelo AdMob.
         initialize(new AgeOfCapybaras(), config);
     }
 }
