bed70f0fbb6cc6f718c75542d6ffbd6c2b9a3087
vitorsamora <vitor.samora@gmail.com>
2015-11-15 18:42:22 -0200

    First accessory screen implementation

---
+++ core/src/com/mygdx/aoc/Accessory.java
@@ -0,0 +1,7 @@
+package com.mygdx.aoc;
+
+/**
+ * Created by vitor on 15/11/15.
+ */
+public class Accessory {
+}
---
+++ core/src/com/mygdx/aoc/screen/AccessoryScreen.java
@@ -0,0 +1,7 @@
+package com.mygdx.aoc.screen;
+
+/**
+ * Created by vitor on 14/11/15.
+ */
+public class AccessoryScreen {
+}
