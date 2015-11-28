60730388f834e1a0baeac8b87397fe74b1fc945a
Yan Couto <yancouto@gmail.com>
2015-11-28 00:48:39 -0200

    Lame Test
    - Just testing JUnit

---
+++ core/test/com/mygdx/aoc/TestTest.java
@@ -0,0 +1,18 @@
+package com.mygdx.aoc;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
